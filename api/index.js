3eb66d6f927e32da30037b72e98bedc7f7e937f8
Yussuf <you@example.com>
2023-04-28 06:53:19 +0100

    another

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
+
+mongoose.set("strictQuery",true)
+try {
+    await mongoose.connect(
+      "mongodb+srv://carissaajao:olatunjiayo@cluster0.xa8k7.mongodb.net/?retryWrites=true&w=majority&dbname=fiverr-clone"
+    );
+    console.log("connected to db");
+} catch (error) {
+    console.log(error);
+}
+
 const PORT = 8000
 app.listen(PORT,()=>{
     console.log(`"Server listening on port ${PORT}`);
